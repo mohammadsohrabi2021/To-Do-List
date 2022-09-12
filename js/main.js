9ac678c01450bb940997eeceb13d3ea22faa4324
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-09-12 22:56:37 +0430

    print database in the body index.html

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ const database = [
     {id:2 , name:'reza',state:false},
     {id:3 , name:'mohammad',state:false},
 ]
+const rander = db =>{
+    database.map(item =>(
+        todocontainer.innerHTML +=`
+        <div class="out">
+           <div>id: ${item.id}</div>
+           <div>name: ${item.name}</div>
+           <div> state:${item.state}</div>
+        </div>
+        `
+    ))
+}
+rander()
