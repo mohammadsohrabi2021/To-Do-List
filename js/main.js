a04f3f0a18a905dfbfbd0b7a5bca30075b97a13d
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-09-12 22:43:03 +0430

    ceate navbar todolist index.html

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const todocontainer =document.querySelector('#todocontainer');
+
+const database = [
+    {id:1 , name:'ali',state:false},
+    {id:2 , name:'reza',state:false},
+    {id:3 , name:'mohammad',state:false},
+]
